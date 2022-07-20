aff18f506c74f736e9b5569b56357fd470f5a93b
STONELAP-2\david <dajoyce.br@gmail.com>
2022-07-20 23:32:33 +0100

    Completed Merge method. Corrected MergeCases test case data

--- SortManager/SortManagerApp/MergeSort.cs
+++ SortManager/SortManagerApp/MergeSort.cs
@@ -8,8 +8,8 @@ public class SortingAlgorithms
 
         int arrSz = inputArr.Length / 2;
 
-        int[] firstArr = new int[arrSz];                        // 1, 5, 6
-        int[] secondArr = new int[inputArr.Length - arrSz];     // 2, 7
+        int[] firstArr = new int[arrSz];                        
+        int[] secondArr = new int[inputArr.Length - arrSz];     
 
         Array.Copy(inputArr, firstArr, arrSz);
         Array.Copy(inputArr, arrSz, secondArr, 0, secondArr.Length);
@@ -24,23 +24,36 @@ public class SortingAlgorithms
     {
         int[] sorted = new int[first.Length + second.Length];
 
-        for (int i = 0, j = 0, k = 0; i < sorted.Length; i++)
+        int i = 0, j = 0, k = 0;
+
+        // While both sorted arrays have elements that haven't been processed yet...
+        while (j < first.Length && k < second.Length) 
         {
-            if (first[j] < second[k])
+            /* Compare the current elements from both arrays.
+             * Set sorted to the lowest of the two values first[j] and second[k]
+             * and increment the sorted array index AND the lowest value index */
+            if (first[j] <= second[k])
             {
-                sorted[i] = first[j];
-                j++;
-            } else if (first[j] > second[k])
-            {
-                sorted[i] = second[k];
-                k++;
+                sorted[i++] = first[j++];
             }
-            else if (first[j] == second[k])
+            else if (first[j] > second[k])
             {
-
+                sorted[i++] = second[k++];
             }
-
         }
+
+        // By now, we have run out of elements from one of the arrays. So just copy the values from the
+        // remaining array into the sorted array (because both first and second arrays are...*sorted*)
+        while (j < first.Length)
+        {
+            sorted[i++] = first[j++];
+        }
+
+        while (k < second.Length)
+        {
+            sorted[i++] = second[k++];
+        }
+
         return sorted;
     }
 }
