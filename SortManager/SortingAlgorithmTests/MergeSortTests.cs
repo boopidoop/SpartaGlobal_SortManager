f637d01f7b782c7e0edc94b6388960faccd4168b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-20 16:56:41 +0100

    Add initial test project with some mergesort tests

---
+++ SortManager/SortingAlgorithmTests/MergeSortTests.cs
@@ -0,0 +1,24 @@
+using SortManagerApp;
+namespace SortingAlgorithmTests
+{
+    public class Tests
+    {
+
+        static object[] SortCases =
+        {
+            new object[] { new int[] { 3,2,1 }, new int[] { 1,2,3 } },
+            new object[] { new int[] { 7,-7,100,4,1 }, new int[] { -7,1,4,7,100 } },
+            new object[] { new int[] { }, new int[] { } },
+            new object[] { new int[] { -8,-23,-15 }, new int[] { -23,-15,-8 } },
+            new object[] { new int[] { 80,25,16 }, new int[] { 16,25,80 } }
+        };
+
+
+        [TestCaseSource(nameof(SortCases))]
+        public void Given_AnUnsortedArray_MergeSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
+        {
+            Assert.That(SortingAlgorithms.MergeSort(inputArray), Is.EqualTo(sortedArray));
+        }
+
+    }
+}
