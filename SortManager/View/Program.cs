5bc33d5911206bd7f16e4ccf43b561588bcd3a1c
LabaDev <labalimbu360@gmail.com>
2022-07-20 18:46:47 +0100

    functions for controller and view completed

--- SortManager/View/Program.cs
+++ SortManager/View/Program.cs
@@ -6,6 +6,10 @@ public class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("SUPER SORTING MACHINE");
+        Console.WriteLine("Type in numbers with spaces to separate the numbers (eg: 1 2 3 4 5)");
+        string numbers =  Console.ReadLine();
+        string listNumbers = ControllerToModel.StringToArray(numbers);
+        Console.WriteLine(listNumbers);
     }
 }
