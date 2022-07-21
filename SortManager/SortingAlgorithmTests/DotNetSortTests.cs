235736a7a8e797a8dbb16478bffe4894df1c257a
BuildTools <unconfigured@null.spigotmc.org>
2022-07-21 17:56:09 +0100

    Fixed DotNET spelling to DotNet

--- SortManager/SortingAlgorithmTests/DotNetSortTests.cs
+++ SortManager/SortingAlgorithmTests/DotNetSortTests.cs
@@ -9,44 +9,44 @@ namespace SortingAlgorithmTests;
 public class DotNetSortTests
 {
     [TestCaseSource(typeof(TestData), nameof(TestData.PositiveInts))]
-    public void Given_AnUnsortedArrayOfPositiveInts_DotNETSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
+    public void Given_AnUnsortedArrayOfPositiveInts_DotNetSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
     {
-        Assert.That(DotNETSort.Sort(inputArray), Is.EqualTo(sortedArray));
+        Assert.That(DotNetSort.Sort(inputArray), Is.EqualTo(sortedArray));
     }
 
     [TestCaseSource(typeof(TestData), nameof(TestData.NegativeInts))]
-    public void Given_AnUnsortedArrayOfNegativeInts_DotNETSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
+    public void Given_AnUnsortedArrayOfNegativeInts_DotNetSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
     {
-        Assert.That(DotNETSort.Sort(inputArray), Is.EqualTo(sortedArray));
+        Assert.That(DotNetSort.Sort(inputArray), Is.EqualTo(sortedArray));
     }
 
     [TestCaseSource(typeof(TestData), nameof(TestData.PositiveAndNegativeInts))]
-    public void Given_AnUnsortedArrayOfPositiveAndNegativeInts_DotNETSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
+    public void Given_AnUnsortedArrayOfPositiveAndNegativeInts_DotNetSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
     {
-        Assert.That(DotNETSort.Sort(inputArray), Is.EqualTo(sortedArray));
+        Assert.That(DotNetSort.Sort(inputArray), Is.EqualTo(sortedArray));
     }
 
     [TestCaseSource(typeof(TestData), nameof(TestData.ReversedInts))]
-    public void Given_AReversedSortedArrayOfInts_DotNETSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
+    public void Given_AReversedSortedArrayOfInts_DotNetSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
     {
-        Assert.That(DotNETSort.Sort(inputArray), Is.EqualTo(sortedArray));
+        Assert.That(DotNetSort.Sort(inputArray), Is.EqualTo(sortedArray));
     }
 
     [TestCaseSource(typeof(TestData), nameof(TestData.SingleLengthArray))]
-    public void Given_ASingleLengthArray_DotNETSort_ReturnsTheSameArray(int[] inputArray, int[] sortedArray)
+    public void Given_ASingleLengthArray_DotNetSort_ReturnsTheSameArray(int[] inputArray, int[] sortedArray)
     {
-        Assert.That(DotNETSort.Sort(inputArray), Is.EqualTo(sortedArray));
+        Assert.That(DotNetSort.Sort(inputArray), Is.EqualTo(sortedArray));
     }
 
     [TestCaseSource(typeof(TestData), nameof(TestData.SameInts))]
-    public void Given_AnArrayOfTheSameInts_DotNETSort_ReturnsTheSameArray(int[] inputArray, int[] sortedArray)
+    public void Given_AnArrayOfTheSameInts_DotNetSort_ReturnsTheSameArray(int[] inputArray, int[] sortedArray)
     {
-        Assert.That(DotNETSort.Sort(inputArray), Is.EqualTo(sortedArray));
+        Assert.That(DotNetSort.Sort(inputArray), Is.EqualTo(sortedArray));
     }
 
     [TestCaseSource(typeof(TestData), nameof(TestData.ExtremeInts))]
-    public void Given_AnArrayWithMaxValueAndMinValueInts_DotNETSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
+    public void Given_AnArrayWithMaxValueAndMinValueInts_DotNetSort_ReturnsSortedArray(int[] inputArray, int[] sortedArray)
     {
-        Assert.That(DotNETSort.Sort(inputArray), Is.EqualTo(sortedArray));
+        Assert.That(DotNetSort.Sort(inputArray), Is.EqualTo(sortedArray));
     }
 }
