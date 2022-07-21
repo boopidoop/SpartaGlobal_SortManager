2378a132dcc54cab1babaada7152d2616336823e
boopidoop <daniel.summerside@yahoo.com>
2022-07-21 20:57:53 +0100

    Finished View, fixed a few bugs with Model, Tests and Controller

--- SortManager/SortManagerApp/BubbleSort.cs
+++ SortManager/SortManagerApp/BubbleSort.cs
@@ -7,28 +7,34 @@ public class BubbleSort : ISort
     public int[] Sort(int[] ints)
     {
         bool sorted = false;
+        int[] result = new int[ints.Length];
+        for (int i = 0; i < ints.Length; i++)
+        {
+            result[i] = ints[i];
+        }
+
         while (!sorted)
         {
             // Switch if next element is lower
-            for (int i = 0; i < ints.Length - 1; i++)
+            for (int i = 0; i < result.Length - 1; i++)
             {
-                if (ints[i] > ints[i + 1])
+                if (result[i] > result[i + 1])
                 {
-                    int swap = ints[i];
-                    ints[i] = ints[i + 1];
-                    ints[i + 1] = swap;
+                    int swap = result[i];
+                    result[i] = result[i + 1];
+                    result[i + 1] = swap;
                 }
             }
 
             // Check if list is sorted
             sorted = true;
-            for (int i = 0; i < ints.Length - 1; i++)
+            for (int i = 0; i < result.Length - 1; i++)
             {
-                if (ints[i] > ints[i + 1])
+                if (result[i] > result[i + 1])
                     sorted = false;
             }
         }
 
-        return ints;
+        return result;
     }
 }
--- SortManager/View/Program.cs
+++ SortManager/View/Program.cs
@@ -5,15 +5,18 @@ namespace View;
 public class Program
 {
     public static string separator = "--------------------------";
+    private static ConsoleColor[] colours = new ConsoleColor[] { ConsoleColor.Blue, ConsoleColor.Magenta, ConsoleColor.Green, ConsoleColor.Yellow };
+    private static Random random;
+
     public static void Main(string[] args)
     {
         var devs = new string[] { "Charlie", "Dan", "David", "Laba" };
-        var colours = new ConsoleColor[] { ConsoleColor.Blue, ConsoleColor.Magenta, ConsoleColor.Green, ConsoleColor.Yellow };
+        random = new Random();
 
         Console.Beep(300, 100);
         Console.WriteLine("Welcome to the Sorting Algorithm Application!");
         Console.Beep(300, 100);
-        Console.Write("Created by: ");
+        Console.Write("\tCreated by: ");
 
         for (int i = 0; i <= 3; i++)
         {
@@ -27,50 +30,135 @@ public class Program
             Console.ResetColor();
             if (i != 3)
                 Console.Write(", ");
+            else
+                Console.WriteLine();
         }
-        Console.WriteLine();
-        Console.WriteLine(separator);
+
         SelectSortingAlgorithm();
     }
 
     public static void SelectSortingAlgorithm()
     {
-        Console.WriteLine("Please choose a Sorting Algorithm");
+        Console.Beep(300, 100);
+        Console.WriteLine(separator);
+        Console.WriteLine("Please choose a Sorting Algorithm from the Following:");
+
+        foreach (var alg in Controls.listOfAlgos)
+        {
+            Console.Beep(500, 100);
+            Console.ForegroundColor = colours[random.Next(colours.Length)];
+            Console.WriteLine(alg.ToString());
+        }
+        Console.ResetColor();
 
         bool valid = false;
         while (!valid)
         {
-            string output = ControllerToModel.CheckAlgorithmInput(Console.ReadLine());
+            Console.Beep(300, 100);
+            Console.Write(" > ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string output = Controls.CheckAlgorithmInput(Console.ReadLine());
+            Console.ResetColor();
+
             if (output != "")
                 Console.WriteLine(output);
             else
                 valid = true;
         }
+
         SetArraySize();
     }
 
     public static void SetArraySize()
     {
+        Console.Beep(300, 100);
+        Console.WriteLine(separator);
         Console.WriteLine("Please enter the size of the array to be sorted");
 
         bool valid = false;
         while (!valid)
         {
-            string output = ControllerToModel.SetArraySize(Console.ReadLine());
+            Console.Beep(300, 100);
+            Console.Write(" > ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string output = Controls.CheckArrayInput(Console.ReadLine());
+            Console.ResetColor();
+
             if (output != "")
                 Console.WriteLine(output);
             else
                 valid = true;
         }
+
+        Confirm();
     }
 
-    public static void GetUnsortedArray()
+    public static void Confirm()
     {
+        Console.Beep(300, 100);
+        Console.WriteLine(separator);
+        Console.WriteLine("Are these details correct? [Y/N]");
+        Console.Beep(500, 100);
+        Console.WriteLine($"\tSort Type: {Controls._sortAlgorithm}");
+        Console.Beep(500, 100);
+        Console.WriteLine($"\tArray Size: {Controls._arraySize}");
 
+        bool valid = false;
+        string output = "";
+        while (!valid)
+        {
+            Console.Beep(300, 100);
+            Console.Write(" > ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            output = Console.ReadLine();
+            Console.ResetColor();
+
+            if (output.ToLower() == "y" || output.ToLower() == "n")
+                valid = true;
+            else
+                Console.WriteLine("Please enter a valid input (Format: [Y/N])");
+        }
+
+        if (output.ToLower() == "y")
+            Console.WriteLine(Controls.Run());
+        if (output.ToLower() == "n")
+            SelectSortingAlgorithm();
+
+        End();
     }
 
-    public static void Restart()
+    public static void End()
     {
-        Console.WriteLine("Would you like to restart?")
+        Console.Beep(300, 100);
+        Console.WriteLine(separator);
+        Console.WriteLine("Would you like to restart or quit?");
+
+        bool valid = false;
+        string output = "";
+        while (!valid)
+        {
+            Console.Beep(300, 100);
+            Console.Write(" > ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            output = Console.ReadLine();
+            Console.ResetColor();
+
+            if (output.ToLower() == "restart" || output.ToLower() == "quit")
+                valid = true;
+            else
+                Console.WriteLine("Please enter either 'restart' or 'quit'");
+        }
+
+
+        if (output.ToLower() == "restart")
+            SelectSortingAlgorithm();
+        else
+        {
+            Console.WriteLine("Thank you for using the application!");
+            for (int i = 500; i > 100; i -= 100)
+            {
+                Console.Beep(i, 100);
+            }
+        }
     }
 }
