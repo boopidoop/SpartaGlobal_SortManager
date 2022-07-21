f54ab7f80298de4b0c0da5cecf188cc263b21d05
BuildTools <unconfigured@null.spigotmc.org>
2022-07-21 19:07:27 +0100

    Add User Input Tests

---
+++ SortManager/SortingAlgorithmTests/UserInputTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithmTests;
+
+public class UserInputTests
+{
+    [TestCase("BubbleSort")]
+    [TestCase("Bubbles")]
+    [TestCase("Bubble time")]
+    [TestCase("gimme a BuBBlE")]
+    [Ignore("test not implemented")]
+    public void GivenBubbleSortString_CheckInput_ReturnsBubbleSort() 
+    {
+        
+    }
+
+    [TestCase("MergeSort")]
+    [TestCase("Merge")]
+    [TestCase("Merge please")]
+    [TestCase("gimme a MeRgE")]
+    [Ignore("test not implemented")]
+    public void GivenMergeSortString_CheckInput_ReturnsMergeSort()
+    {
+        
+    }
+
+    [TestCase("DotNetSort")]
+    [TestCase("DotNet")]
+    [TestCase("Dotnet please")]
+    [TestCase("gimme a DOTnET")]
+    [Ignore("test not implemented")]
+    public void GivenDotNetSortString_CheckInput_ReturnsDotNetSort()
+    {
+        
+    }
+
+    [TestCase("x")]
+    [TestCase("")]
+    [TestCase("gimme something")]
+    [Ignore("test not implemented")]
+    public void GivenInvalidString_CheckInput_ReturnsMessage()
+    {
+        
+    }
+}
